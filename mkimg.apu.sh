7c5b8e0f3143ebfbb14ac2aaf9b177d6960423cd
Alessandro Ratti <alessandro.ratti@gmail.com>
2021-12-08 11:40:33 +0100

    Add iperf

--- mkimg.apu.sh
+++ mkimg.apu.sh
@@ -2,6 +2,6 @@ profile_apu() {
         profile_extended
         kernel_cmdline="unionfs_size=512M console=tty0 console=ttyS0,115200"
         syslinux_serial="0 115200"
-        apks="$apks mdadm zsh iw dmidecode flashrom
+        apks="$apks mdadm zsh iw dmidecode flashrom iperf
 	     "
 }
